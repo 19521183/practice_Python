97c827207aaded9ef0248342085ae97f54b51104
An Nguyễn Thị Thúy <19521183@gm.uit.edu.vn>
2020-11-13 16:18:18 +0700

    update main

--- practice Python/Tuples/Tuples.py
+++ practice Python/Tuples/Tuples.py
@@ -1,1 +1,10 @@
-
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = map(int, input().split())
+    # convert a list to a tuple
+    integer_tuple = tuple(integer_list)
+    # create an empty tuple then add items of a list into it
+    # integer_tuple = ()
+    # for item in integer_list:
+    #   integer_tuple += (item,) 
+    print(hash(integer_tuple))
