edf916e11eec59ff05b45b786eae0edf03e601d4
An Nguyễn Thị Thúy <19521183@gm.uit.edu.vn>
2020-11-13 15:51:28 +0700

    update main

--- practice Python/Dictionary_and_List/Dictionary_and_List.py
+++ practice Python/Dictionary_and_List/Dictionary_and_List.py
@@ -1,1 +1,17 @@
+def get_average(scores):
+    sum=0
+    for item in scores:
+        sum+=item
+    return float(sum)/len(scores)
 
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for i in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    average = get_average(student_marks.get(query_name))
+    print('{:.2f}'.format(average))
+    
